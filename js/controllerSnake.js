98ffdd6cb86d6f3d42946150101fed487a28d81b
Tom DEPREZ <tom_deprez@hotmail.fr>
2025-05-15 16:37:33 +0200

    code a debuger

--- js/controllerSnake.js
+++ js/controllerSnake.js
@@ -1,9 +1,51 @@
 let snake = document.getElementById("snake");
 let positionDeMonSnake = 0;
 
-setInterval(mouveSnake, 100); 
 
-function mouveSnake() {
+function mouveSnakeDown() {
+    positionDeMonSnake += 10;
+    snake.style.top = positionDeMonSnake + "px";
+}
+
+function mouveSnakeUp() {
+    positionDeMonSnake -= 10;
+    snake.style.top = positionDeMonSnake + "px";
+}
+
+function mouveSnakeLeft() {
+    positionDeMonSnake -= 10;
+    snake.style.left = positionDeMonSnake + "px";
+}
+
+function mouveSnakeRight() {
     positionDeMonSnake += 10;
     snake.style.left = positionDeMonSnake + "px";
+}
+
+let keydownSave = "";
+
+document.addEventListener('keydown', function (event) {
+    keydownSave = event.key
+});
+
+setInterval(mouveSnake(keydownSave), 100);
+
+function mouveSnake(keydown){
+    if (keydown == "ArrowRight") {
+        mouveSnakeRight();
+        
+    }
+    if (keydown == "ArrowLeft") {
+        mouveSnakeLeft();
+        
+    }
+    if (keydown == "ArrowUp") {
+        mouveSnakeUp();
+        
+    }
+    if (keydown == "ArrowDown") {
+        mouveSnakeDown();
+        
+    }
+    
 }
